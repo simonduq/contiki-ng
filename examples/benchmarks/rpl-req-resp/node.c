7996689efa24bd19341688d802eeb940594f6851
Simon Duquennoy <simon.duquennoy@ri.se>
2018-05-17 06:04:36 -0700

    More work

--- examples/benchmarks/rpl-req-resp/node.c
+++ examples/benchmarks/rpl-req-resp/node.c
@@ -45,14 +45,49 @@
 /* Log configuration */
 #include "sys/log.h"
 #define LOG_MODULE "App"
-#define LOG_LEVEL LOG_LEVEL_DBG
+#define LOG_LEVEL LOG_LEVEL_INFO
 
+#define UDP_PORT 8214
 #define SEND_INTERVAL (CLOCK_SECOND)
+
+static struct simple_udp_connection udp_conn;
+struct app_payload {
+  unsigned id;
+  unsigned is_request;
+};
 
 /*---------------------------------------------------------------------------*/
 PROCESS(app_process, "App process");
 AUTOSTART_PROCESSES(&app_process);
 
+/*---------------------------------------------------------------------------*/
+static void
+udp_rx_callback(struct simple_udp_connection *c,
+         const uip_ipaddr_t *sender_addr,
+         uint16_t sender_port,
+         const uip_ipaddr_t *receiver_addr,
+         uint16_t receiver_port,
+         const uint8_t *data,
+         uint16_t datalen)
+{
+  struct app_payload message;
+  memcpy(&message, data, sizeof(message));
+
+  if(message.is_request) {
+    LOG_INFO("Received request %u from ", message.id);
+    LOG_INFO_6ADDR(sender_addr);
+    LOG_INFO_("\n");
+    message.is_request = 0;
+    LOG_INFO("Sending response %u to ", message.id);
+    LOG_INFO_6ADDR(sender_addr);
+    LOG_INFO_("\n");
+    simple_udp_sendto(&udp_conn, &message, sizeof(message), sender_addr);
+  } else {
+    LOG_INFO("Received response %u from ", message.id);
+    LOG_INFO_6ADDR(sender_addr);
+    LOG_INFO_("\n");
+  }
+}
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(app_process, ev, data)
 {
@@ -60,6 +95,10 @@ PROCESS_THREAD(app_process, ev, data)
   static uip_ipaddr_t dest_ipaddr;
 
   PROCESS_BEGIN();
+
+  /* Initialize UDP connection */
+  simple_udp_register(&udp_conn, UDP_PORT, NULL,
+                      UDP_PORT, udp_rx_callback);
 
   /* Setup a periodic timer that expires after 10 seconds. */
   etimer_set(&timer, CLOCK_SECOND * 10);
@@ -71,8 +110,6 @@ PROCESS_THREAD(app_process, ev, data)
     NETSTACK_ROUTING.get_root_ipaddr(&dest_ipaddr);
     /* Wait until all nodes have joined */
     do {
-      uip_sr_node_t *link;
-
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
       etimer_reset(&timer);
 
@@ -81,17 +118,6 @@ PROCESS_THREAD(app_process, ev, data)
       }
       LOG_INFO("Node count: %u/%u\n", uip_sr_num_nodes(), deployment_node_cont());
 
-      /* Print our routing */
-      LOG_DBG("Reachable nodes (%u in total):\n", uip_sr_num_nodes());
-      link = uip_sr_node_head();
-      while(link != NULL) {
-        uip_ipaddr_t node_ipaddr;
-        NETSTACK_ROUTING.get_sr_node_ipaddr(&node_ipaddr, link);
-        LOG_DBG("-- ");
-        LOG_DBG_6ADDR(&node_ipaddr);
-        LOG_DBG_("\n");
-        link = uip_sr_node_next(link);
-      }
     } while(uip_sr_num_nodes() < deployment_node_cont());
 
     /* Now start requesting nodes at random */
@@ -99,6 +125,7 @@ PROCESS_THREAD(app_process, ev, data)
     while(1) {
       static unsigned count = 0;
       uint16_t dest_id;
+      struct app_payload message;
 
       PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
       etimer_reset(&timer);
@@ -110,9 +137,12 @@ PROCESS_THREAD(app_process, ev, data)
       /* Prefix was already set, set IID now */
       deployment_iid_from_id(&dest_ipaddr, dest_id);
 
-      LOG_INFO("Sending to ");
+      message.id = count;
+      message.is_request = 1;
+      LOG_INFO("Sending request %u to ", message.id);
       LOG_INFO_6ADDR(&dest_ipaddr);
-      LOG_INFO_(", count %u\n", count);
+      LOG_INFO_("\n");
+      simple_udp_sendto(&udp_conn, &message, sizeof(message), &dest_ipaddr);
       count++;
     }
   }
