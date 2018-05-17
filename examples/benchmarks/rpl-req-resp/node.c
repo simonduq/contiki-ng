80072cd2f3673796de92eecee24f0a7716a60a2a
Simon Duquennoy <simon.duquennoy@ri.se>
2018-05-17 01:19:59 -0700

    Added examples sub-directory for benchmarks

---
+++ examples/benchmarks/rpl-req-resp/node.c
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2018, RISE SICS.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ *
+ */
+
+/**
+ * \file
+ *         Benchmark: the root sends requests to all nodes in a randomized
+ *         order, and receives resopnses back.
+ * \author
+ *         Simon Duquennoy <simon.duquennoy@ri.se>
+ */
+
+#include "contiki.h"
+#include "contiki-net.h"
+#include "services/deployment/deployment.h"
+
+/* Log configuration */
+#include "sys/log.h"
+#define LOG_MODULE "App"
+#define LOG_LEVEL LOG_LEVEL_INFO
+
+/*---------------------------------------------------------------------------*/
+PROCESS(app_process, "App process");
+AUTOSTART_PROCESSES(&app_process);
+
+/*---------------------------------------------------------------------------*/
+PROCESS_THREAD(app_process, ev, data)
+{
+  static struct etimer timer;
+
+  PROCESS_BEGIN();
+
+  /* Setup a periodic timer that expires after 10 seconds. */
+  etimer_set(&timer, CLOCK_SECOND * 10);
+
+  while(1) {
+    uip_ds6_addr_t *lladdr;
+
+    LOG_INFO("Node ID: %u\n", node_id);
+    LOG_INFO("Link-layer address: ");
+    LOG_INFO_LLADDR(&linkaddr_node_addr);
+    LOG_INFO_("\n");
+    lladdr = uip_ds6_get_link_local(-1);
+    LOG_INFO("Tentative link-local IPv6 address: ");
+    LOG_INFO_6ADDR(lladdr != NULL ? &lladdr->ipaddr : NULL);
+    LOG_INFO_("\n");
+
+    /* Wait for the periodic timer to expire and then restart the timer. */
+    PROCESS_WAIT_EVENT_UNTIL(etimer_expired(&timer));
+    etimer_reset(&timer);
+  }
+
+  PROCESS_END();
+}
+/*---------------------------------------------------------------------------*/
---
+++ examples/benchmarks/rpl-req-resp/project-conf.h
@@ -0,0 +1,7 @@
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+#define DEPLOYMENT_MAPPING deployment_sics_firefly
+#define LOG_CONF_WITH_COMPACT_ADDR 1
+
+#endif /* PROJECT_CONF_H_ */
