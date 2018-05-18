026b03adeee40b2faecf3a612c93cf85a9428c6e
Simon Duquennoy <simon.duquennoy@ri.se>
2018-05-18 13:59:50 -0700

    Parsing script

--- examples/benchmarks/rpl-req-resp/parse.py
+++ examples/benchmarks/rpl-req-resp/parse.py
@@ -16,6 +16,30 @@ pd.set_option('display.max_rows', 48)
 pd.set_option('display.width', None)
 pd.set_option('display.max_columns', None)
 
+parents = {}
+
+def calculateHops(node):
+    hops = 0
+    while(parents[node] != None):
+        node = parents[node]
+        hops += 1
+    return hops
+
+def calculateChildren(node):
+    children = 0
+    for n in parents.keys():
+        if(parents[n] == node):
+            children += 1
+    return children
+
+def updateTopology(child, parent):
+    global parents
+    if not child in parents:
+        parents[child] = {}
+    if not parent in parents:
+        parents[parent] = None
+    parents[child] = parent
+
 def parseRPL(log):
     res = re.compile('.*? rank (\d*).*?nbr count (\d*)').match(log)
     if res:
@@ -25,7 +49,17 @@ def parseRPL(log):
     res = re.compile('parent switch: .*? -> .*?-(\d*)$').match(log)
     if res:
         parent = int(res.group(1))
-        return {'event': 'switch', 'parent': parent }
+        return {'event': 'switch', 'pswitch': parent }
+    res = re.compile('links: 6G-(\d+)\s*to 6G-(\d+)').match(log)
+    if res:
+        child = int(res.group(1))
+        parent = int(res.group(2))
+        updateTopology(child, parent)
+        return None
+    res = re.compile('links: end of list').match(log)
+    if res:
+        # This was the last line, commit full topology
+        return {'event': 'topology' }
     return None
 
 def parseEnergest(log):
@@ -47,7 +81,7 @@ def parseApp(log):
         type = res.group(1)
         id = int(res.group(2))
         dest = int(res.group(3))
-        return {'event': 'send', 'type': type, 'id': id, 'dest': dest }
+        return {'event': 'send', 'type': type, 'id': id, 'node': dest }
     res = re.compile('Received (.+?) (\d+) from 6G-(\d+)').match(log)
     if res:
         type = res.group(1)
@@ -78,6 +112,7 @@ def doParse(dir):
         "energest": [],
         "ranks": [],
         "switches": [],
+        "topology": [],
     }
 
     print("\nProcessing %s" %(file))
@@ -93,39 +128,46 @@ def doParse(dir):
             print("%u, "%(time / 60),end='', flush=True)
             lastPrintedTime = time
 
+        entry = {
+            "timestamp": timedelta(seconds=time),
+            "node": nodeid,
+        }
+
         if module == "App":
             ret = parseApp(log)
             if(ret != None):
+                entry.update(ret)
                 if(ret['event'] == 'send' and ret['type'] == 'request'):
                     # populate series of sent requests
-                    ret["src"] = nodeid
-                    ret["timestamp"] = timedelta(seconds=time)
-                    ret['received'] = False
-                    arrays["packets"].append(ret)
+                    entry['pdr'] = 0.
+                    arrays["packets"].append(entry)
                 elif(ret['event'] == 'recv' and ret['type'] == 'response'):
-                    # update sent request series with latency and received flag
+                    # update sent request series with latency and PDR
                     txElement = [x for x in arrays["packets"] if x['event']=='send' and x['id']==ret['id']][0]
                     txElement['latency'] = time - txElement['timestamp'].seconds
-                    txElement['received'] = True
+                    txElement['pdr'] = 100.
 
         if module == "Energest":
             ret = parseEnergest(log)
             if(ret != None):
-                ret["node"] = nodeid
-                ret["timestamp"] = timedelta(seconds=time)
-                arrays["energest"].append(ret)
+                entry.update(ret)
+                arrays["energest"].append(entry)
 
         if module == "RPL":
             ret = parseRPL(log)
             if(ret != None):
+                entry.update(ret)
                 if(ret['event'] == 'rank'):
-                    ret["node"] = nodeid
-                    ret["timestamp"] = timedelta(seconds=time)
-                    arrays["ranks"].append(ret)
+                    arrays["ranks"].append(entry)
                 elif(ret['event'] == 'switch'):
-                    ret["node"] = nodeid
-                    ret["timestamp"] = timedelta(seconds=time)
-                    arrays["switches"].append(ret)
+                    arrays["switches"].append(entry)
+                elif(ret['event'] == 'topology'):
+                    for n in parents.keys():
+                        nodeEntry = entry.copy()
+                        nodeEntry["node"] = n
+                        nodeEntry["hops"] = calculateHops(n)
+                        nodeEntry["children"] = calculateChildren(n)
+                        arrays["topology"].append(nodeEntry)
 
     print("")
 
@@ -137,6 +179,15 @@ def doParse(dir):
 
     return dfs
 
+def outputStats(df, metric, agg):
+    perNode = getattr(df[["node", metric]].groupby("node"), agg)()
+    print("%s, for each node: " %(metric))
+    print(perNode[metric].as_matrix())
+
+    perTime = getattr(df[["node", metric]].groupby([pd.Grouper(freq="2Min")]), agg)()
+    print("%s, timeline: " %(metric))
+    print(perTime[metric].as_matrix())
+
 def main():
     if len(sys.argv) < 1:
         return
@@ -147,80 +198,18 @@ def main():
     # Parse the original log
     dfs = doParse(dir)
 
-    ###########################################################
-    # PDR
-    ###########################################################
-    print("Latency, for each packet: ")
-    print(dfs["packets"]['latency'].as_matrix())
+    # Output relevant metrics
+    outputStats(dfs["packets"], "pdr", "mean")
+    outputStats(dfs["packets"], "latency", "mean")
 
-    perNode = dfs["packets"].groupby("dest").agg({'received': {'received': 'sum', 'sent': 'count'}})
-    perNode.columns = perNode.columns.droplevel(0)
-    perNode["pdr"] =  100 * perNode["received"] / perNode["sent"]
+    outputStats(dfs["energest"], "duty-cycle", "mean")
+    outputStats(dfs["energest"], "channel-utilization", "mean")
 
-    print("PDR, for each node: ")
-    print(perNode['pdr'].as_matrix())
+    outputStats(dfs["ranks"], "rank", "mean")
 
-    perTime = dfs["packets"].groupby([pd.Grouper(freq="2Min")]).agg({'received': {'received': 'sum', 'sent': 'count'}})
-    perTime.columns = perTime.columns.droplevel(0)
-    perTime["pdr"] =  100 * perTime["received"] / perTime["sent"]
+    outputStats(dfs["switches"], "pswitch", "count")
 
-    print("PDR, timeline: ")
-    print(perTime['pdr'].as_matrix())
-
-    ###########################################################
-    # Latency
-    ###########################################################
-    print("Latency, for each packet: ")
-    print(dfs["packets"]['latency'].as_matrix())
-
-    perNode = dfs["packets"][["dest", "latency"]].groupby("dest").mean()
-    print("Latency, for each node: ")
-    print(perNode['latency'].as_matrix())
-
-    perTime = dfs["packets"][["dest", "latency"]].groupby([pd.Grouper(freq="2Min")]).mean()
-    print("Latency, timeline: ")
-    print(perTime['latency'].as_matrix())
-
-    ###########################################################
-    # Energest
-    ###########################################################
-
-    perNode = dfs["energest"].groupby("node").mean()
-    print("Duty Cycle, for each node: ")
-    print(perNode['duty-cycle'].as_matrix())
-
-    print("Channel Utilization, for each node: ")
-    print(perNode['channel-utilization'].as_matrix())
-
-    perTime = dfs["energest"].groupby([pd.Grouper(freq="2Min")]).mean()
-    print("Duty Cycle, timeline: ")
-    print(perTime['duty-cycle'].as_matrix())
-
-    print("Channel Utilization, timeline: ")
-    print(perTime['channel-utilization'].as_matrix())
-
-    ###########################################################
-    # Ranks
-    ###########################################################
-    perNode = dfs["ranks"].groupby("node").mean()
-    print("Ranks, for each node: ")
-    print(perNode['rank'].as_matrix())
-    # all info for a given nbr: dfs["ranks"][(dfs["ranks"].node==1)]["neighbors"].as_matrix()
-
-    perTime = dfs["ranks"].groupby([pd.Grouper(freq="2Min")]).mean()
-    print("Ranks, timeline: ")
-    print(perTime['rank'].as_matrix())
-
-    ###########################################################
-    # Parent switches
-    ###########################################################
-    perNode = dfs["switches"].groupby("node").count()
-    print("Ranks, for each node: ")
-    print(perNode['parent'].as_matrix())
-    # all info for a given nbr: dfs["ranks"][(dfs["ranks"].node==1)]["neighbors"].as_matrix()
-
-    perTime = dfs["switches"].groupby([pd.Grouper(freq="2Min")]).count()
-    print("Ranks, timeline: ")
-    print(perTime['parent'].as_matrix())
+    outputStats(dfs["topology"], "hops", "mean")
+    outputStats(dfs["topology"], "children", "mean")
 
 main()
