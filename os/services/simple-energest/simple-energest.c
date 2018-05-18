b6b5a3db63a73fd403427afb2ed9c975b7b16506
Simon Duquennoy <simon.duquennoy@ri.se>
2018-05-18 14:19:22 -0700

    cr

--- os/services/simple-energest/simple-energest.c
+++ os/services/simple-energest/simple-energest.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 201, Swedish Institute of Computer Science.
+ * Copyright (c) 2018, RISE SICS.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
