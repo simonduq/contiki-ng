9161a406c024a73fd8aecb03ac32d2db2f1f9413
Simon Duquennoy <simon.duquennoy@ri.se>
2018-05-20 02:11:24 -0700

    Benchmark: configurable builds

--- examples/benchmarks/rpl-req-resp/project-conf.h
+++ examples/benchmarks/rpl-req-resp/project-conf.h
@@ -8,14 +8,18 @@
 
 /* Logging */
 #define LOG_CONF_LEVEL_RPL LOG_LEVEL_INFO
+#define LOG_CONF_LEVEL_MAC LOG_LEVEL_DBG
 #define LOG_CONF_WITH_COMPACT_ADDR 1
 
 /* Provisioning */
 #define NETSTACK_MAX_ROUTE_ENTRIES 25
 #define NBR_TABLE_CONF_MAX_NEIGHBORS 8
 
+#if CONFIG_OPTIMS == 1
+
 /* RPL configuration */
 #define RPL_MRHOF_CONF_SQUARED_ETX 1
+
 /* Five nines reliability paper used the config below */
 // #define RPL_CONF_DIO_INTERVAL_MIN 14 /* 2^14 ms = 16.384 s */
 // #define RPL_CONF_DIO_INTERVAL_DOUBLINGS 6 /* 2^(14+6) ms = 1048.576 s */
@@ -24,10 +28,12 @@
 /* TSCH configuration */
 #define TSCH_CONF_RX_WAIT 1000
 #define ORCHESTRA_CONF_UNICAST_PERIOD 7
+
 /* Five nines reliability paper used the config below */
 // #define TSCH_CONF_KEEPALIVE_TIMEOUT (20 * CLOCK_SECOND)
 // #define TSCH_CONF_MAX_KEEPALIVE_TIMEOUT (60 * CLOCK_SECOND)
 // #define TSCH_CONF_EB_PERIOD (16 * CLOCK_SECOND)
 // #define TSCH_CONF_MAX_EB_PERIOD (50 * CLOCK_SECOND)
+#endif
 
 #endif /* PROJECT_CONF_H_ */
